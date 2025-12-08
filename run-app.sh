63651d3d6207027553e89f26bd07eaa381bcf6be
Robert Gunczer <rgunczer@gmail.com>
2025-12-08 04:09:21 +0100

    use allexport to safely load env vars

--- run-app.sh
+++ run-app.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-export $(cat .env | xargs)
+set -o allexport
+source .env
+set +o allexport
+
+# uncomment to view the exported variables from the .env file
+#printenv | grep -E '^(DBUTIL_)' | sort || echo "No matching vars found"
+
 mvn spring-boot:run
